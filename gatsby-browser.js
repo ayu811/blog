80e25dabc4879be23a2206d980247e14f5ed69fd
ayu811 <ayuhaya.create@gmail.com>
2024-12-27 15:25:19 +0900

    first commit

---
+++ gatsby-browser.js
@@ -0,0 +1,3 @@
+import './src/css/main.css'
+import './src/css/bootstrap-grid.min.css'
+import 'prismjs/themes/prism-tomorrow.css'
---
+++ gatsby-config.js
@@ -0,0 +1,74 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+const path = require('path')
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+module.exports = {
+  pathPrefix: `/`,
+  siteMetadata: {
+    title: "Ayu's Blog",
+    titleTemplate: '',
+    // titleTemplate: "%s - Curated List of Free Education Resources",
+    description:
+      '量子コンピューターやAIなどの幅広い先端技術とプログラミングに関する知見をまとめて書いているサイト',
+    url: '', // No trailing slash allowed!
+    image: '/logo.png', // Path to the image placed in the 'static' folder, in the project's root directory.
+    twitterUsername: '',
+    defaultTitle: 'Revista',
+  },
+
+  plugins: [
+    `gatsby-plugin-netlify`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-postcss`,
+    `gatsby-read-time-estimate`,
+    `gatsby-transformer-remark`,
+    // {
+    //   resolve: 'gatsby-plugin-robots-txt',
+    //   options: {
+    //     host: 'https://mysite.com',
+    //     sitemap: 'https://mysite.com/sitemap.xml',
+    //     policy: [{ userAgent: '*', allow: '/' }],
+    //   },
+    // },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        content: [
+          path.join(process.cwd(), 'src/**/!(*.d).{ts,js,jsx,tsx,md,mdx}'),
+        ],
+      },
+    },
+
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/src/assets`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/content`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [{ resolve: 'gatsby-remark-images' }],
+      },
+    },
+  ],
+}
